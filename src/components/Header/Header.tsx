f2242fcc739cd911330938dec39e6479ab8cfb51
ceaiius <85124227+ceaiius@users.noreply.github.com>
2025-05-27 17:21:01 +0400

    refactor: add custom colors in tailwind

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,30 +24,30 @@ const Header = () => {
 
           <div className='flex-shrink min-w-0'>
             <a href="#">
-              <h1 className="text-2xl font-bold text-[#E52020]">Frag</h1>
+              <h1 className="text-2xl font-bold text-red-default">Frag</h1>
             </a>
           </div>
 
           <div className="relative hidden md:flex w-[34vw] lg:w-[42vw] max-w-[704px]">
             <img src={searchIcon} className="absolute left-6 w-5 h-full" alt="search" />
             <input type="text" placeholder="Search"
-              className="h-10 rounded-3xl bg-[#F3F3F3] text-ellipsis border-2 border-[#262626] w-full px-12" />
+              className="h-10 rounded-3xl bg-black-button text-ellipsis border-2 border-black-border w-full px-12" />
           </div>
 
           <nav>
             <ul className="flex gap-4 items-center">
               <li>
-                <button className="bg-[#262626] text-white px-4 py-1 rounded-xs font-bold hidden md:block">Sell now</button>
+                <button className="bg-black-border text-white px-4 py-1 rounded-xs font-bold hidden md:block">Sell now</button>
               </li>
-              <li className='md:hidden'><img src={searchIcon} className="w-7 h-7" /></li>
-              <li><a href="#"><img src={inboxIcon} className='w-7 h-7' /></a></li>
-              <li><a href="#"><img src={heartIcon} className='w-7 h-7' /></a></li>
-              <li><a href="#"><img src={bagIcon} className='w-7 h-7' /></a></li>
+              <li className='md:hidden'><img src={searchIcon} className="w-7 h-7" alt='search icon'/></li>
+              <li><a href="#"><img src={inboxIcon} className='w-7 h-7' alt='inbox icon'/></a></li>
+              <li><a href="#"><img src={heartIcon} className='w-7 h-7' alt='heart Icon'/></a></li>
+              <li><a href="#"><img src={bagIcon} className='w-7 h-7' alt='bag Icon'/></a></li>
               <li className='hidden md:block'><a href="#">Profile</a></li>
             </ul>
           </nav>
         </div>
-        <hr className='bg-[#d7d7d7] border-0 h-px' />
+        <hr className='bg-light-border border-0 h-px' />
 
         <div className='hidden md:block'>
           <Navbar />
@@ -66,7 +66,7 @@ const Header = () => {
         ${menuOpen ? 'translate-x-0' : '-translate-x-full'}`}
       >
         <div className="flex justify-between items-center p-4 border-b border-gray-200">
-          <h2 className="text-xl font-bold text-[#E52020]">Frag</h2>
+          <h2 className="text-xl font-bold text-red-default">Frag</h2>
           <button onClick={() => setMenuOpen(false)} className="text-2xl font-bold">&times;</button>
         </div>
         <Navbar isMobile />
--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = ({ isMobile = false }) => {
     return (
-      <nav className={`${isMobile ? 'p-6' : 'px-4 py-0 border border-[#eeeeee]'}`}>
+      <nav className={`${isMobile ? 'p-6' : 'px-4 py-0 border border-white-border'}`}>
         <ul className={`list-none ${isMobile ? 'flex flex-col gap-6 text-lg font-semibold' : 'flex gap-8 font-bold text-lg p-4'}`}>
           <li><a href="#">Men</a></li>
           <li><a href="#">Women</a></li>
